841c6ea42af273dcbb818cbb3f16ba790b8f5c1f
Juliet-Stan <julietstanislaus402@gmail.com>
2025-08-29 20:04:16 +0100

    Added database.py

---
+++ Student Management System/database.py
@@ -0,0 +1,17 @@
+#database.py
+
+from sqlmodel import SQLModel, create_engine, Session
+import os
+
+# SQLite database URL
+DATABASE_URL = "sqlite:///./students.db"
+
+# Create engine
+engine = create_engine(DATABASE_URL, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
