3db19d6d0168321c7f56867da2fb1af40d7a6084
Juliet-Stan <julietstanislaus402@gmail.com>
2025-08-29 22:08:36 +0100

    Added database.py

---
+++ Notes API/database.py
@@ -0,0 +1,17 @@
+#database.py
+
+
+from sqlmodel import SQLModel, create_engine, Session
+
+# SQLite database URL
+DATABASE_URL = "sqlite:///./notes.db"
+
+# Create engine
+engine = create_engine(DATABASE_URL, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
